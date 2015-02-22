7108e2c0d2f44748fc885913c1c18dc6471257b7
pchin <pchin@medstudy.com>
2015-02-22 17:16:27 -0600

    Move admim routes out to a routeAdmin.php

---
+++ app/routes/routeAdmin.php
@@ -0,0 +1,45 @@
+<?php
+
+/** ------------------------------------------
+ *  Admin Routes
+ *  ------------------------------------------
+ */
+Route::group(array('prefix' => 'admin', 'before' => 'auth'), function()
+{
+
+    # Comment Management
+    Route::get('comments/{comment}/edit', 'AdminCommentsController@getEdit');
+    Route::post('comments/{comment}/edit', 'AdminCommentsController@postEdit');
+    Route::get('comments/{comment}/delete', 'AdminCommentsController@getDelete');
+    Route::post('comments/{comment}/delete', 'AdminCommentsController@postDelete');
+    Route::controller('comments', 'AdminCommentsController');
+
+    # Blog Management
+    Route::get('blogs/{post}/show', 'AdminBlogsController@getShow');
+    Route::get('blogs/{post}/edit', 'AdminBlogsController@getEdit');
+    Route::post('blogs/{post}/edit', 'AdminBlogsController@postEdit');
+    Route::get('blogs/{post}/delete', 'AdminBlogsController@getDelete');
+    Route::post('blogs/{post}/delete', 'AdminBlogsController@postDelete');
+    Route::controller('blogs', 'AdminBlogsController');
+
+    # User Management
+    Route::get('users/{user}/show', 'AdminUsersController@getShow');
+    Route::get('users/{user}/edit', 'AdminUsersController@getEdit');
+    Route::post('users/{user}/edit', 'AdminUsersController@postEdit');
+    Route::get('users/{user}/delete', 'AdminUsersController@getDelete');
+    Route::post('users/{user}/delete', 'AdminUsersController@postDelete');
+    Route::controller('users', 'AdminUsersController');
+
+    # User Role Management
+    Route::get('roles/{role}/show', 'AdminRolesController@getShow');
+    Route::get('roles/{role}/edit', 'AdminRolesController@getEdit');
+    Route::post('roles/{role}/edit', 'AdminRolesController@postEdit');
+    Route::get('roles/{role}/delete', 'AdminRolesController@getDelete');
+    Route::post('roles/{role}/delete', 'AdminRolesController@postDelete');
+    Route::controller('roles', 'AdminRolesController');
+
+    # Admin Dashboard
+    Route::controller('/', 'AdminDashboardController');
+});
+
+
