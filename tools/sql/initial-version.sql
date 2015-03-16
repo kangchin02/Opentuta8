b4feae0aeb99de3a855896706b9e7d535c99f214
pchin <pchin@medstudy.com>
2015-03-16 14:50:57 -0500

    make facebook_uid column nullable

--- tools/sql/initial-version.sql
+++ tools/sql/initial-version.sql
@@ -7,7 +7,7 @@ CREATE TABLE `users` (
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `facebook_uid` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `facebook_uid` bigint(20) unsigned DEFAULT NULL,
   `google_uid` bigint(20) unsigned NOT NULL DEFAULT '0',
   `confirmation_code` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `remember_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
