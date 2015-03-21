0bd2834059e5dbf6ae4eaf095545d243cc3fd4ad
pchin <pchin@medstudy.com>
2015-03-20 22:22:02 -0500

    update sql to make both facebook and google to large field

--- tools/sql/initial-version.sql
+++ tools/sql/initial-version.sql
@@ -3,19 +3,21 @@
 -- Add users table
 -- ********************************************************************
 CREATE TABLE `users` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `facebook_uid` bigint(20) unsigned DEFAULT NULL,
-  `google_uid` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `facebook_uid` decimal(32,0) unsigned DEFAULT NULL,
   `confirmation_code` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `remember_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `confirmed` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `createdby` int(10) unsigned DEFAULT NULL,
-  `modifiedby` int(10) unsigned DEFAULT NULL,
-  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `avatar` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `google_uid` decimal(32,0) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `laravel`.`users` ADD COLUMN `avatar` VARCHAR(255) DEFAULT NULL;
+
+
